7c4192444fbe568634269156e88e3413414c8226
assietoundiaye <133644666+assietoundiaye@users.noreply.github.com>
2025-05-20 20:04:44 +0000

    Version-1 commit 4

--- Version 1/article.php
+++ Version 1/article.php
@@ -41,7 +41,7 @@ $categories = $query->fetchAll(PDO::FETCH_ASSOC);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title><?= htmlspecialchars($article['titre']) ?> - École Supérieure Polytechnique</title>
+    <title><?= htmlspecialchars($article['titre']) ?> -- École Supérieure Polytechnique</title>
     <link rel="stylesheet" href="css/index2.css">
 </head>
 <body>
