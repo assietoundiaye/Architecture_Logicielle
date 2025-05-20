dccb8ff1e05b9e47763b85f22ad29bda2ab095b7
assietoundiaye <133644666+assietoundiaye@users.noreply.github.com>
2025-05-20 20:03:34 +0000

    Version-2 commit 3

--- Version 2/views/home.php
+++ Version 2/views/home.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Actualités - École Supérieure Polytechnique</title>
+    <title>Page d'acceuil</title>
     <link rel="stylesheet" href="../views/css/index2.css">
 </head>
 <body>
