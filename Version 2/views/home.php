51fdd6a6aa5f5a3f7675262e4b1e9b415cb8946c
assietoundiaye <133644666+assietoundiaye@users.noreply.github.com>
2025-05-21 00:05:17 +0000

    Update home.php

--- Version 2/views/home.php
+++ Version 2/views/home.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Page d'accueil</title>
+    <title>Accueil</title>
     <link rel="stylesheet" href="../views/css/index2.css">
 </head>
 <body>
